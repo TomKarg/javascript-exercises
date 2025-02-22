8b13a91bf15b6516915f5c7c9cb0a424876c0e17
TomKarg <22795138+TomKarg@users.noreply.github.com>
2025-02-22 16:27:26 +0100

    solve exercise 01-07

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function (textArray, ...theArgs) {
+  const retArr = textArray.filter((i) => !theArgs.includes(i));
+  return retArr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+  if (!Number.isInteger(min) || !Number.isInteger(max) || min < 1 || max < 1) {
+    return "ERROR";
+  }
 
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+
+  let sum = 0;
+  for (min; min <= max; min++) {
+    sum += min;
+  }
+  return sum;
 };
 
 // Do not edit below this line
