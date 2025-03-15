9ad908aa6352d21b5300e4ced4710f114bda05ab
TomKarg <22795138+TomKarg@users.noreply.github.com>
2025-03-15 23:01:28 +0900

    solve challenge 11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldest = people.reduce((acc, person) => 
+    {
+        if(!person.hasOwnProperty('yearOfDeath')){
+            person.yearOfDeath = new Date().getFullYear();
+        }
+        const age = person.yearOfDeath - person.yearOfBirth;
+        if(acc.yearOfDeath - acc.yearOfBirth > age){
+            return acc;
+        } else {
+            return person;
+        }
+    }, {})
+    return oldest;
 };
 
 // Do not edit below this line
